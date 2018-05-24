bdde2cfa85a0ecf92ffa892052104d24232745b7
Duane Kord <dukord@microsoft.com>
2018-05-24 10:37:11 -0700

    Working version.  Needs UI.

--- cosmosui/Controllers/HomeController.cs
+++ cosmosui/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using cosmosui.Data;
@@ -9,13 +13,16 @@ namespace cosmosui.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private IEnumerable<FieldMasterInfo> items;
+        private Dictionary<string, IEnumerable<object>> _dropdowns;
+        private PropertyInfo[] properties;
+
+        public async Task<ActionResult> Index()
         {
-            ViewData["tenants"] = GetTenants();
+            items = await DocumentDBRepository<FieldMasterInfo>.GetAll(d => d.TenantId != null);
 
-
-
-            return View();
+            //populate dropdowns
+            return View(SetUp(items));
         }
 
         [Authorize(Roles = "Observer")]
@@ -38,34 +45,69 @@ namespace cosmosui.Controllers
         [HttpPost]
         public async Task<ActionResult> Index(FieldMasterInfo fminfo)
         {
-            //Set up repository and pre-defined tenants
-            ViewData["tenants"] = GetTenants();
-            DocumentDBRepository<FieldMasterInfo>.Initialize();
+            //Set up
             IEnumerable<FieldMasterInfo> items;
+            Type myType = fminfo.GetType();
+            properties = fminfo.GetType().GetProperties();
+            var predicate = PredicateBuilder.True<FieldMasterInfo>();
 
-            if (string.IsNullOrEmpty(fminfo.InputType))
+            foreach (var i in fminfo.store_dd)
             {
-                items = await DocumentDBRepository<FieldMasterInfo>.GetItemsAsync(d => d.FieldId.Equals(fminfo.FieldId), fminfo.TenantId);
-                return View();
-            }
-            if (!string.IsNullOrEmpty(fminfo.InputType))
-            {
-                items = await DocumentDBRepository<FieldMasterInfo>.GetItemsAsyncMultiParams(d => d.FieldId.Equals(fminfo.FieldId), d => d.InputType.Equals(fminfo.InputType), fminfo.TenantId);
-                return View();
+                if (i.Value != null && i.Value != "")
+                {
+                    foreach (PropertyInfo property in properties)
+                    {
+                        //var localI = i;
+                        //FieldMasterInfo localFMI = fminfo;
+
+                        if (i.Key == property.Name)
+                        {
+                            var value = fminfo.store_dd[i.Key];
+                            var expr = GenerateFieldExpression<FieldMasterInfo>(property.Name, value);
+
+                            myType.GetProperty(property.Name).SetValue(fminfo, value);
+                            predicate = predicate.And(expr);
+                            break;
+                        }
+                    }
+                }
             }
 
-            return View();
+            items = await DocumentDBRepository<FieldMasterInfo>.GetItemsAsync(predicate, fminfo.TenantId);
+
+            return View("~/Views/Home/ResultsView.cshtml", items);
         }
 
-        public static SelectList GetTenants()
+
+        //Populates dropdowns based on DropDown attribute above class property
+        public FieldMasterInfo SetUp(IEnumerable<FieldMasterInfo> allItems)
         {
+            FieldMasterInfo fmi = new FieldMasterInfo();
+            this.properties = fmi.GetType().GetProperties();
 
-            var tenants = new List<SelectListItem>();
+            foreach (PropertyInfo property in properties)
+            {
+                if(Attribute.IsDefined(property, typeof(DropdownAttribute)))
+                {
+                    fmi.populate_dd.Add(property.Name, allItems.Select(m => m.GetType().GetProperty(property.Name).GetValue(m, null)).Distinct());
+                }
+            }
+            return fmi;
+        }
 
-            tenants.Add(new SelectListItem { Text = "CI", Value = "CI" });
-            tenants.Add(new SelectListItem { Text = "AP", Value = "AP" });
-
-            return new SelectList(tenants, "Value", "Text");
+        public Expression<Func<T, bool>> GenerateFieldExpression<T>(string fieldName, string value)
+        {
+            var parameter = Expression.Parameter(typeof(T), "m");
+            // m
+            var fieldAccess = Expression.PropertyOrField(parameter, fieldName);
+            // m.[fieldName]
+            var nullValue = Expression.Constant(value);
+            // null
+            var body = Expression.Equal(fieldAccess, nullValue);
+            // m.[fieldName] == value
+            var expr = Expression.Lambda<Func<T, bool>>(body, parameter);
+            // m => m.[fieldName] == value
+            return expr;
         }
     }
 }
