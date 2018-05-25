e8b3e4ed0b54ee51206f626f33640dfd8b338703
Duane Kord <dukord@microsoft.com>
2018-05-24 17:36:56 -0700

    Framework Fixes.

--- cosmosui/Controllers/BankController.cs
+++ cosmosui/Controllers/BankController.cs
@@ -1,22 +1,22 @@
-﻿using System;
+﻿using cosmosui.Data;
+using cosmosui.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Reflection;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
-using cosmosui.Data;
-using cosmosui.Models;
 
 namespace cosmosui.Controllers
 {
     [Authorize]
-    public class HomeController : Controller
+    public class BankController : Controller
     {
-        private IEnumerable<FieldMasterInfo> items;
-        private Dictionary<string, IEnumerable<object>> _dropdowns;
         private PropertyInfo[] properties;
+        private IEnumerable<FieldMasterInfo> items;
 
+        // GET: Bank
         public async Task<ActionResult> Index()
         {
             items = await DocumentDBRepository<FieldMasterInfo>.GetAll(d => d.TenantId != null);
@@ -25,23 +25,7 @@ namespace cosmosui.Controllers
             return View(SetUp(items));
         }
 
-        [Authorize(Roles = "Observer")]
-        public ActionResult Observer()
-        {
-            ViewBag.Message = "You can see this because you are an observer.";
-
-            return View();
-        }
-
-        [Authorize(Roles ="Admin")]
-        public ActionResult Admin()
-        {
-            ViewBag.Message = "You can see this because you are an admin.";
-
-            return View();
-        }
-
-        //[Authorize(Roles = "Admin")]
+        //POST
         [HttpPost]
         public async Task<ActionResult> Index(FieldMasterInfo fminfo)
         {
@@ -78,8 +62,6 @@ namespace cosmosui.Controllers
             return View("~/Views/Home/ResultsView.cshtml", items);
         }
 
-
-        //Populates dropdowns based on DropDown attribute above class property
         public FieldMasterInfo SetUp(IEnumerable<FieldMasterInfo> allItems)
         {
             FieldMasterInfo fmi = new FieldMasterInfo();
@@ -87,12 +69,14 @@ namespace cosmosui.Controllers
 
             foreach (PropertyInfo property in properties)
             {
-                if(Attribute.IsDefined(property, typeof(DropdownAttribute)))
+                if (Attribute.IsDefined(property, typeof(DropdownAttribute)))
                 {
                     fmi.populate_dd.Add(property.Name, allItems.Select(m => m.GetType().GetProperty(property.Name).GetValue(m, null)).Distinct());
                 }
             }
             return fmi;
         }
+
+
     }
 }
---
+++ cosmosui/Controllers/TaxController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cosmosui.Controllers
+{
+    public class TaxController : Controller
+    {
+        // GET: Tax
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
